af9c25db121746b5e5dc3b8d83bce85196812bd3
liudong0763 <308315409@qq.com>
2017-05-13 22:06:29 +0800

    fix pip8

--- team/admin.py
+++ team/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Team
+from .models import User, Team
 
 # Register your models here.
 
-class UserAdmin(UserAdmin):
-	list_display = ('username', 'last_login', 'phone', 'is_active')
+
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', )
+
 
 class TeamAdmin(admin.ModelAdmin):
-	list_display = ('name', 'leader_name')
+    list_display = ('name', 'leader_name', )
 
-	def leader_name(self, obj):
-		# print(obj.leader.values_list('username', flat=True))
-		return obj.leader.values_list('username').get()
+    def leader_name(self, obj):
+        # print(obj.leader.values_list('username', flat=True))
+        return obj.leader.values_list('username').get()
 
-	leader_name.short_description = '领队'
+    leader_name.short_description = '领队'
 
-admin.site.register(User,UserAdmin)
+admin.site.register(User,MyUserAdmin)
 admin.site.register(Team,TeamAdmin)
